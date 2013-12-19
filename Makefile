37bcd35ece3840494d8df9f1e16101ce4719133c
Kamil Michalak <kmichalak8@gmail.com>
2013-12-19 23:19:19 +0100

    Add unit tests for StringUtils

--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ INCLUDE = -I/usr/local/include
 LDFLAGS = -L/usr/local/lib
 LDLIBS  = -lcurl
 
+TEST_LDFLAGS = -L/usr/local/gtest-1.6.0/lib
+TEST_LDLIBS = -lgtest
+
 all: jippi
 
 jippi: main.o restclient.o stringutils.o
@@ -16,10 +19,24 @@ main.o: main.cpp
 	$(CC) $(CFLAGS) $(CXXFLAGS) main.cpp
 	
 restclient.o:
-	$(CC) $(CFLAGS) $(CXXFLAGS) rest/client/restclient.cpp
+	$(CC) $(CFLAGS) $(CXXFLAGS) src/rest/client/restclient.cpp
 	
 stringutils.o:
-	$(CC) $(CFLAGS) $(CXXFLAGS) utils/string/stringutils.cpp
+	$(CC) $(CFLAGS) $(CXXFLAGS) src/utils/string/stringutils.cpp
+	
+	
+	
+	
+tests: runtests.o stringutilstest.o
+	$(CC) $(INCLUDE) $(CXXFLAGS) $(TEST_LDFLAGS) $(TEST_LDLIBS) runtests.o stringutilstest.o -o test-all 
+	
+runtests.o: test/runtests.cpp
+	$(CC) $(CFLAGS) $(CXXFLAGS) test/runtests.cpp
+	
+stringutilstest.o: 
+	$(CC) $(CFLAGS) $(CXXFLAGS) test/utils/string/stringutilstest.cpp
+	
+	
 	
 clean:
-	rm -rf *.o jippi
+	rm -rf *.o jippi test-all
---
+++ test/utils/string/stringutilstest.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2013 <copyright holder> <email>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+#include <gtest/gtest.h>
+#include "include/stringutilstest.hpp"
+#include "../../../src/utils/string/stringutils.cpp"
+
+
+TEST(StringUtilsTest, TrimCutsLeadingSpaces)
+{
+	// given 
+	const std::string & test_string = std::string(" test string");	
+	// when
+	std::string result = StringUtils::trim(test_string);	
+	// then
+	EXPECT_EQ("test string", result);
+}
+
+
+TEST(StringUtilsTest, TrimCutsTrailingSpaces)
+{
+	// given 
+	const std::string & test_string = std::string("test string ");
+	// when
+	std::string result = StringUtils::trim(test_string);
+	// then
+	EXPECT_EQ("test string", result);
+}
+
+
+TEST(StringUtilsTest, TrimCutsLeadingAndTrailingSpaces)
+{
+	// given 
+	const std::string & test_string = std::string(" test string ");
+	// when
+	std::string result = StringUtils::trim(test_string);
+	// then
+	EXPECT_EQ("test string", result);
+}
+
+
+TEST(StringUtilsTest, IsEmptyReturnsTrueWhenStringIsEmpty)
+{
+	// given 
+	const std::string test_string;
+	// when
+	bool result = StringUtils::is_empty(test_string);
+	// then
+	ASSERT_TRUE(result);
+}
+
+
+TEST(StringUtilsTest, IsEmptyReturnsFalseWhenStringIsNotEmpty)
+{
+	// given 
+	const std::string test_string = std::string("test string");
+	// when
+	bool result = StringUtils::is_empty(test_string);
+	// then
+	ASSERT_FALSE(result);
+}
